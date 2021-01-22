7b8973da0f6578fafc600db5385f54dd5a23e75f
take UE <uesugi1118@gmail.com>
2021-01-22 15:24:52 +0900

    constrained change

--- database/migrations/2020_09_04_213144_create_responses_table.php
+++ database/migrations/2020_09_04_213144_create_responses_table.php
@@ -16,7 +16,7 @@ class CreateResponsesTable extends Migration
         Schema::create('responses', function (Blueprint $table) {
             $table->id();
             $table->integer('response')->nullable(false)->comment('反応');
-            $table->foreignId('answer_id')->constrained('')->onDelete('cascade')->onUpdate('cascade')->comment('回答欄ID');
+            $table->foreignId('answer_id')->constrained('answers')->onDelete('cascade')->onUpdate('cascade')->comment('回答欄ID');
             $table->timestamps();
             $table->index('id');
             $table->index('answer_id');
